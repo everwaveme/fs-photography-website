d8d187a2bde543dfdbb0ed0a23bb94fda21d0625
Farhad Sadykov <f.sadykov@yahoo.com>
2024-02-04 01:46:01 +0300

    js script for burger menu, adaptive for header

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -2,3 +2,20 @@
 AOS.init({
   once: true,
 });
+
+//Open/Close burger menu
+const burger = document.querySelector('.js-header-burger');
+const nav = document.querySelector('.js-nav');
+const body = document.querySelector('body');
+
+burger.addEventListener('click', () => {
+  // nav.classList.toggle('active'); // как варик, только покороче
+  // body.classList.toggle('lock'); // как варик, только покороче
+  if (nav.classList.contains('active') == true) {
+    nav.classList.remove('active');
+    body.classList.remove('lock');
+  } else {
+    nav.classList.add('active');
+    body.classList.add('lock');
+  }
+});
